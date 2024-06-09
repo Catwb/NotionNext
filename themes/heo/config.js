7b9be1509bc5417b1a1eddb896b5885dd7fa71e0
qiyao <111560834+Catwb@users.noreply.github.com>
2024-06-09 21:11:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,9 +22,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://test-blogq.netlify.app/',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '', url: '' },
+  HEO_HERO_CATEGORY_2: { title: '', url: '' },
+  HEO_HERO_CATEGORY_3: { title: '', url: '' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -41,7 +41,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/Catwb/', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
